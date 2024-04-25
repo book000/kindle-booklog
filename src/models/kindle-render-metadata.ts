bc169d1d71c845d8ec13bf9e095220d71dcbf8ba
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2024-04-26 02:55:19 +0900

    feat: v3

---
+++ src/models/kindle-render-metadata.ts
@@ -0,0 +1,11 @@
+export interface KindleRenderMetadata {
+  bookTitle: string
+  lang: string
+  authors: string[]
+  firstPositionId?: number
+  lastPositionId: number
+  coverPosistion: number
+  writingMode: string
+  direction: string
+  progressionDirection: string
+}
